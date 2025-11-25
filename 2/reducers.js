daeefb8a8ba41ac7508b537f544b0d629c16f358
Udashkins <king_of_hell1@mail.ru>
2025-11-25 22:51:31 +0300

    Update reducers.js

--- 2/reducers.js
+++ 2/reducers.js
@@ -3,13 +3,41 @@ import { combineReducers } from "redux";
 
 const comments = (state = {}, action) => {
   // BEGIN (write your solution here)
-  
+  switch (action.type) {
+    case 'TASK_COMMENT_ADD':
+      return {
+        ...state,
+        [action.payload.comment.id]: action.payload.comment
+      };
+    
+    case 'TASK_COMMENT_REMOVE':
+      return _.omit(state, action.payload.id);
+    
+    case 'TASK_REMOVE':
+      // Удаляем все комментарии с taskId равным id удаляемой задачи
+      return _.omitBy(state, comment => comment.taskId === action.payload.id);
+    
+    default:
+      return state;
+  }  
   // END
 };
 
 const tasks = (state = {}, action) => {
   // BEGIN (write your solution here)
-
+switch (action.type) {
+    case 'TASK_ADD':
+      return {
+        ...state,
+        [action.payload.task.id]: action.payload.task
+      };
+    
+    case 'TASK_REMOVE':
+      return _.omit(state, action.payload.id);
+    
+    default:
+      return state;
+  }
   // END
 };
 
