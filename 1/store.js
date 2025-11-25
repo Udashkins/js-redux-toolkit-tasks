ee63cd3d91d4e314beb54329a3eadbb87dbc254a
Udashkins <king_of_hell1@mail.ru>
2025-11-25 22:50:10 +0300

    Update store.js

--- 1/store.js
+++ 1/store.js
@@ -2,5 +2,23 @@ import omit from "lodash/omit";
 import { legacy_createStore as createStore } from "redux";
 
 // BEGIN (write your solution here)
+const tasksReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'TASK_ADD':
+      return {
+        ...state,
+        [action.payload.task.id]: action.payload.task
+      };
 
+    case 'TASK_REMOVE':
+      return omit(state, action.payload.id);
+
+    default:
+      return state;
+  }
+};
+
+export default (initialState = {}) => {
+  return createStore(tasksReducer, initialState);
+};
 // END
